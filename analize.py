d9ab7228e4e2c0051923ca789ed55a25346ceaed
ryperson <80601374+ry207@users.noreply.github.com>
2025-01-25 16:49:07 -0600

    Update analize.py

--- analize.py
+++ analize.py
@@ -3,6 +3,9 @@ import speech_recognition as sr
 import os
 import colorconsole.terminal
 import random
+import datetime
+import simpleaudio as sa
+
 
 from wikipage import getpage
 from forum import news
@@ -10,10 +13,13 @@ from wiki import analizevoice
 from sshtoself import sshto
 from dictionary import getdef
 from papers import getpapers
+from stocks import getstocks
+from phrases import *
 
 # init function to get an engine instance for the speech synthesis 
 engine = pyttsx3.init()
 r = sr.Recognizer()
+
 
 random_facts = [
     "Bananas are berries, but strawberries are not.",
@@ -25,21 +31,40 @@ random_facts = [
     "Wombat poop is cube-shaped to prevent it from rolling away.",
     "The human nose can detect over 1 trillion different scents.",
     "Cleopatra lived closer in time to the first moon landing than to the construction of the Great Pyramid of Giza.",
-    "Sharks have been around longer than trees. They have existed for over 400 million years."
+    "Sharks have been around longer than trees. They have existed for over 400 million years.",
+    "A single cloud can weigh more than a million pounds.",
+    "There are more possible iterations of a game of chess than there are atoms in the known universe.",
+    "Sloths can hold their breath longer than dolphins by slowing their heart rate to stay underwater for up to 40 minutes.",
+    "The shortest war in history lasted only 38 to 45 minutes between Britain and Zanzibar in 1896.",
+    "The inventor of the frisbee was turned into a frisbee after he died.",
+    "Hot water freezes faster than cold water, a phenomenon known as the Mpemba effect.",
+    "There’s enough DNA in the human body to stretch from the Earth to the Sun and back about 600 times.",
+    "An octopus has blue blood because it uses copper instead of iron to transport oxygen.",
+    "Butterflies can taste with their feet to identify the right plants to lay their eggs on.",
+    "Venus is the only planet in our solar system that spins clockwise.",
+    "A group of flamingos is called a 'flamboyance.'",
+    "There’s a species of jellyfish that can revert its cells to an earlier stage, making it potentially immortal.",
+    "Humans share about 60% of their DNA with bananas.",
+    "The word 'alphabet' comes from the first two letters of the Greek alphabet: alpha and beta.",
+    "A bolt of lightning is five times hotter than the surface of the sun.",
+    "Penguins propose to their mates by presenting them with a perfect pebble.",
+    "If you could fold a piece of paper 42 times, it would reach the Moon.",
+    "You can’t hum while holding your nose closed.",
+    "The heart of a blue whale is so large that a human could swim through its arteries.",
+    "There are more fake flamingos in the world than real ones."
 ]
 
-greetings = ["Hello", "hello", "hello Jarvis", "jarvis", "hey Jarvis", "Jarvis", "come in Jarvis", "how are you", "how are you Jarvis", "what's up"]
-good = ["Good", "good", "great", "okay", "okay jarvis", "thanks", "thanks jarvis", "okay jarvis", "Great", "I'm good", "i'm good", "im good", "i'm okay"]
-bad = ["bad", "not good", "no", "No"]
-function1 = ["function"]
-todo = ["what to do", "to do", "to-do", "Todo", "To-Do"]
-new_project = ["new project", "project", "start a new project", "Jarvis start a new project", "make a folder", "new folder", "start a project"]
+
+
+
 
 def analize(voice):
-    if voice == "shut down":
+    if voice in exit_shutdown:
         engine.say("Goodbye, Sir")
         engine.runAndWait()
         exit(69)
+    elif voice in stock_phrases:
+        getstocks()
     elif voice == "news":
         news()
         engine.say("Heres the news, sir.")
@@ -60,13 +85,24 @@ def analize(voice):
             except:
                 engine.say("An error occured")
                 engine.runAndWait()
-    elif voice == "random":
-        randnum = random.randrange(0,10)
+    elif voice in facts:
+        randnum = random.randrange(0,30)
         random_fact = random_facts[randnum]
         engine.say(random_fact)
         engine.runAndWait()
         print(random_fact)
-    elif voice == "joke":
+    elif voice in time_date:
+        x = datetime.datetime.now()
+        print(x.strftime("%A"))
+        print(x.strftime("%B"), x.strftime("%d"))
+        print(x.strftime("%I"), x.strftime("%M"), x.strftime("%p"))
+        hour = x.strftime("%I")
+        hour = hour.replace("0", "")
+        minute = x.strftime("%M")
+        minute = minute.replace("0", "")
+        engine.say(f"Today is {x.strftime("%A")}, {x.strftime("%B")} {x.strftime("%d")}, and it {hour} {minute}, {x.strftime("%p")}")
+        engine.runAndWait()
+    elif voice in fun:
         os.system("curl ascii.live/can-you-hear-me")
         os.system("clear")
     elif voice == "color":
@@ -86,13 +122,16 @@ def analize(voice):
         engine.say("Here is your active and running tasks tree, sir")
         engine.runAndWait()
     elif voice in todo != -1:
-        engine.say('Nothing to do sir')
+        engine.say('Nothing to do sir, lets have a nice day.')
         engine.runAndWait()
     elif voice in greetings != -1:
-        engine.say('Hello sir, how may I help you, sir.')
+        engine.say('Hello sir.')
         engine.runAndWait()
     elif voice in good != -1:
         engine.say('Of course, sir.')
+        engine.runAndWait()
+    elif voice in howis != -1:
+        engine.say('I am well, sir.')
         engine.runAndWait()
     elif voice in bad != -1:
         engine.say('Sorry, Sir.')
@@ -140,7 +179,7 @@ A broad range of industrial and consumer products use computers as control syste
         foldername = input("What should this folder be named: ")
         os.system(f"mkdir ~/{foldername}")
     elif voice == "terminal":
-        name = input("Whats the device name: ")
+        name = input("what's the device name: ")
         engine.say("Goodbye for now sir.")
         engine.runAndWait()
         sshto(name)
@@ -161,7 +200,7 @@ A broad range of industrial and consumer products use computers as control syste
         if mov == "v":
             os.system(f"yt-dlp -o \"%(title)s\".mp4 {url}")
     elif voice == "help":
-        print("Commands: help, shut down, system, tree, random, search, download, joke, color, news, computer, who am I, hack, terminal, new project, movie, get video, memory, clear, definition, research, execute")
+        print("Commands: help, shut down, system, tree, random, search, download, joke, color, news, computer, who am I, hack, terminal, new project, movie, get video, memory, clear, definition, research, execute, time, stocks")
         engine.say("How can i help sir.")
         engine.runAndWait()
     elif voice == "memory":
@@ -169,6 +208,11 @@ A broad range of industrial and consumer products use computers as control syste
             os.system("btop")
         except:
             print("Cannot run this command.")
+    elif voice == "system":
+        command = input("What command would you like to run: ")
+        engine.say("What command would you like to run?")
+        engine.runAndWait()
+        os.system(command)
     elif voice == "clear":
         os.system("clear")
     elif voice == "definition":
@@ -188,7 +232,7 @@ A broad range of industrial and consumer products use computers as control syste
 
 def analizetext(comm):
     if comm in todo != -1:
-        engine.say('Nothing to do sir')
+        engine.say('Nothing to do sir, lets have a nice day')
         engine.runAndWait()
     elif comm == "hack":
         level = input("Select level 0-34: ")
@@ -211,7 +255,7 @@ def analizetext(comm):
         engine.runAndWait()
         getpage(sttsearch)
     elif comm in greetings != -1:
-        engine.say('Hello sir, how may I help you, sir.')
+        engine.say('Hello sir.')
         engine.runAndWait()
         print("How may I help you sir?")
     elif comm in good != -1:
@@ -222,6 +266,9 @@ def analizetext(comm):
         engine.say('Sorry, Sir.')
         engine.runAndWait()
         print("Sorry sir.")
+    elif comm in howis != -1:
+        engine.say('I am well, sir.')
+        engine.runAndWait()
     elif comm in function1 != -1:
         engine.say('What word would you like?')
         engine.runAndWait()
@@ -242,7 +289,7 @@ A broad range of industrial and consumer products use computers as control syste
         search = input("Search: ")
         sttsearch = search.replace(" ", "_")
         analizevoice(sttsearch)
-    elif comm == "joke":
+    elif comm in fun:
         os.system("curl ascii.live/can-you-hear-me")
         os.system("clear")
     elif comm == "color":
@@ -261,32 +308,29 @@ A broad range of industrial and consumer products use computers as control syste
         os.system("pstree")
         engine.say("Here is your active and running tasks tree, sir")
         engine.runAndWait()
-    elif comm == "random":
+    elif comm in facts:
         randnum = random.randrange(0,10)
         random_fact = random_facts[randnum]
         engine.say(random_fact)
         engine.runAndWait()
         print(random_fact)
-    elif comm == "joke":
-        os.system("curl ascii.live/can-you-hear-me")
-        os.system("clear")
     elif comm in new_project != -1:
         engine.say("What shall this folder be named")
         engine.runAndWait()
         foldername = input("What should this folder be named: ")
         os.system(f"mkdir ~/{foldername}")
     elif comm == "terminal":
-        name = input("Whats the device name: ")
+        name = input("what's the device name: ")
         engine.say("Goodbye for now sir.")
         engine.runAndWait()
         sshto(name)
         engine.say("Welcome back, sir.")
         engine.runAndWait()
     elif comm == "help":
-        print("Commands: help, shut down, system, tree, random, search, download, joke, color, news, computer, who am I, hack, terminal, new project, movie, get video, memory, clear, definition, research, execute")
+        print("Commands: help, shut down, system, tree, random, search, download, joke, color, news, computer, who am I, hack, terminal, new project, movie, get video, memory, clear, definition, research, execute, time, stocks")
         engine.say("How can i help sir.")
         engine.runAndWait()
-    elif comm == "shut down":
+    elif comm in exit_shutdown:
         engine.say("Goodbye sir")
         engine.runAndWait()
         exit(69)
@@ -320,6 +364,8 @@ A broad range of industrial and consumer products use computers as control syste
         os.system("ls Executables/")
         executablename = input("Name of executable: ")
         os.system(f".\Executables\{executablename}")
+    elif comm in getstocks:
+        getstocks()
     else:
         engine.say("Not quite sure what you mean, sir.") 
         engine.runAndWait()
