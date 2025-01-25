5b2d2cb088692dec837e77adeb3a446f9c362621
ryperson <80601374+ry207@users.noreply.github.com>
2025-01-25 12:15:27 -0600

    Update wikipage.py

--- wikipage.py
+++ wikipage.py
@@ -7,14 +7,19 @@ def getpage(voice):
     page = requests.get(url).text
     soup = BeautifulSoup(page, "lxml")
     page = soup.find('div', class_='mw-body-content')
+    print(f"URL: {url}")
     try:
-        f = open(f"WikiPages/{voice}_wikipage.html", "w")
-        f.write(f"Wikipedia page for: {voice}\n\n\n{page}")
-        f.close()
-        startlh(voice)
+        #f = open(f"WikiPages/{voice}_wikipage.html", "w")
+        #f.write(f"Wikipedia page for: {voice}\n\n\n{page}")
+        #f.close()
+        #startlh(voice)
+        with open(f"WikiPages/{voice}_wikipage.html", "w", encoding = 'utf-8') as file: 
+            # prettify the soup object and convert it into a string   
+            file.write(str(page.prettify()))
+        startlh()
     except:
         print("An error may have occured.")
 
-def startlh(voice):
+def startlh():
     os.system(f"python -m http.server 8000 -d WikiPages/ --bind 127.0.0.1 ")
 
