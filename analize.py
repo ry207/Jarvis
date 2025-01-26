58ba8c4f0ea5acda52766601a9e09ad73aff9950
ryperson <80601374+ry207@users.noreply.github.com>
2025-01-26 17:20:10 +0000

    Update analize.py

--- analize.py
+++ analize.py
@@ -184,7 +184,7 @@ A broad range of industrial and consumer products use computers as control syste
         if mov == "v":
             os.system(f"yt-dlp -o \"%(title)s\".mp4 {url}")
     elif voice == "help":
-        print("Commands: help, shut down, system, tree, random, search, download, joke, color, news, computer, who am I, hack, terminal, new project, movie, get video, memory, clear, definition, research, execute, time, stocks")
+        print("Commands: help, shut down, system, tree, random, search, download, joke, color, news, computer, who am I, hack, terminal, new project, movie, get video, memory, clear, definition, research, execute, time, stocks, find youtube video")
         engine.say("How can i help sir.")
         engine.runAndWait()
     elif voice == "memory":
@@ -332,7 +332,7 @@ A broad range of industrial and consumer products use computers as control syste
         engine.say("Welcome back, sir.")
         engine.runAndWait()
     elif comm == "help":
-        print("Commands: help, shut down, system, tree, random, search, download, joke, color, news, computer, who am I, hack, terminal, new project, movie, get video, memory, clear, definition, research, execute, time, stocks")
+        print("Commands: help, shut down, system, tree, random, search, download, joke, color, news, computer, who am I, hack, terminal, new project, movie, get video, memory, clear, definition, research, execute, time, stocks, find youtube video")
         engine.say("How can i help sir.")
         engine.runAndWait()
     elif comm in time_date:
