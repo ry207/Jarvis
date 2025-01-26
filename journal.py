7e017b4e4cd73826cfc40da3dd1d42ead0ba409e
ryperson <80601374+ry207@users.noreply.github.com>
2025-01-26 11:56:38 -0600

    Add files via upload

---
+++ journal.py
@@ -0,0 +1,15 @@
+import datetime
+
+journal_entry = input("Journal Entry: ")
+
+x = datetime.datetime.now()
+hour = x.strftime("%I")
+hour = hour.replace("0", "")
+minute = x.strftime("%M")
+
+try:
+    with open("Journal.txt", "a") as f:
+        f.write(f"{x.strftime("%A")}, {x.strftime("%B")} {x.strftime("%d")} {hour}:{minute} {x.strftime("%p")} >> {journal_entry}\n")
+    print("Wrote to Journal.txt")
+except:
+    print("An error may have occured.")
