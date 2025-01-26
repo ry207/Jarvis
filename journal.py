0c54d719714c6a829b2172632998b7544423343c
ryperson <80601374+ry207@users.noreply.github.com>
2025-01-26 16:29:21 -0600

    Update journal.py

--- journal.py
+++ journal.py
@@ -1,15 +1,15 @@
 import datetime
 
-journal_entry = input("Journal Entry: ")
+def addToJournal(journal_entry):
 
-x = datetime.datetime.now()
-hour = x.strftime("%I")
-hour = hour.replace("0", "")
-minute = x.strftime("%M")
+    x = datetime.datetime.now()
+    hour = x.strftime("%I")
+    hour = hour.replace("0", "")
+    minute = x.strftime("%M")
 
-try:
-    with open("Journal.txt", "a") as f:
-        f.write(f"{x.strftime("%A")}, {x.strftime("%B")} {x.strftime("%d")} {hour}:{minute} {x.strftime("%p")} >> {journal_entry}\n")
-    print("Wrote to Journal.txt")
-except:
-    print("An error may have occured.")
+    try:
+        with open("Journal.txt", "a") as f:
+            f.write(f"{x.strftime("%A")}, {x.strftime("%B")} {x.strftime("%d")} {hour}:{minute} {x.strftime("%p")} >> {journal_entry}\n")
+        print("Wrote to Journal.txt")
+    except:
+        print("An error may have occured.")
