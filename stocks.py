bc473e71a9a4593a946a7f48bd209d1b8716c222
ryperson <80601374+ry207@users.noreply.github.com>
2025-01-27 07:27:09 -0600

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -15,13 +15,16 @@ def getstocks():
     
 
     blocks = content.find_all('tr')
-    for block in blocks:
+    for block in reversed(blocks[1:(len(blocks))]):
         tds = block.find_all('td')
         for td in (tds):
-            print(td.text)
+            try:
+                print(td.text)
+            except:
+                print("Error")
         name = block.find('td', class_="datatable-v2_cell__IwP1U !h-auto w-full py-2 mdMax:border-r dynamic-table-v2_col-name__Xhsxv !py-2")
         price = block.find('td', class_="datatable-v2_cell__IwP1U dynamic-table-v2_col-other__zNU4A text-right rtl:text-right")
         percentChange = block.find('td', class_="datatable-v2_cell__IwP1U datatable-v2_cell--up__lVyET datatable-v2_cell--bold__cXQUV dynamic-table_col-other__Eu_RC text-right font-bold rtl:text-right")
         print("----------------------------------")
-    engine.say(f"{name.text} has a price of {price.text} dollars and regular market change of {percentChange.text}")
+    engine.say(f"{name.text} has a price of {price.text} dollars.")
     engine.runAndWait()
