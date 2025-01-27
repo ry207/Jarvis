099493a8f06fe667f41db6d2ec76755712500aab
ryperson <80601374+ry207@users.noreply.github.com>
2025-01-27 19:44:56 +0000

    Update phrases.py

--- phrases.py
+++ phrases.py
@@ -1,15 +1,15 @@
 # User greetings and responses
 greetings = [
-    "Hello", "hello", "hello Jarvis", "jarvis", 
-    "Jarvis", "come in Jarvis", "hey", "okay Jarvis", "yo", 
+    "Hello", "hello", "hello Jarvis", "jarvis",
+    "Jarvis", "come in Jarvis", "hey", "okay Jarvis", "yo",
     "hi Jarvis", "sup Jarvis", "hey there", "morning Jarvis", "Jarvis", "hey Jarvis", "good morning Jarvis", "good morning", "morning", "morning Jarvis", "good afternoon Jarvis", "good afternoon", "good evening Jarvis", "good evening"
 ]
 
 # Positive responses
 good = [
-    "Good", "good", "great", "okay", "okay jarvis", "okay jarvis", "Great", "okay good", 
-    "i'm good", "I'm good", "I'm okay", "cool", "oh cool", 
-    "cool Jarvis", "nice", "nice Jarvis", "that's cool", 
+    "Good", "good", "great", "okay", "okay jarvis", "okay jarvis", "Great", "okay good",
+    "i'm good", "I'm good", "I'm okay", "cool", "oh cool",
+    "cool Jarvis", "nice", "nice Jarvis", "that's cool",
     "awesome", "fantastic", "sweet", "perfect", "amazing", "interesting", "very interesting", "pretty cool", "pretty interesting", "interesting stuff", "bet", "alright", "that's fine", "that's alright", "ok", "that's fantastic"
 ]
 
@@ -17,79 +17,119 @@ thanks = ["wow thanks Jarvis", "thanks Jarvis", "gee thanks", "gee thanks Jarvis
 
 # Asking how Jarvis is
 howis = [
-    "how are you", "how are you Jarvis", "what's up", 
-    "hows it going", "what's up Jarvis", "you good", 
-    "everything okay", "how do you feel", "what's happening", 
+    "how are you", "how are you Jarvis", "what's up",
+    "hows it going", "what's up Jarvis", "you good",
+    "everything okay", "how do you feel", "what's happening",
     "hru", "u good Jarvis", "what's going on", "how are you Jarvis"
 ]
 
 # Negative responses
 bad = [
-    "bad", "not good", "no", "No", "bad Jarvis", "no Jarvis", 
-    "that's not what I meant", "not what I meant", "no thanks", 
-    "stupid", "stupid Jarvis", "dumb", "you're stupid", 
-    "annoying", "go away", "stop it", "you're wrong", 
+    "bad", "not good", "no", "No", "bad Jarvis", "no Jarvis",
+    "that's not what I meant", "not what I meant", "no thanks",
+    "stupid", "stupid Jarvis", "dumb", "you're stupid",
+    "annoying", "go away", "stop it", "you're wrong",
     "not helpful", "ugh", "Jarvis you're useless", "I'm tired", "I'm bored", "nothing to do", "I'm so bored", "Jarvis no", "Jarvis that not what I meant", "not what I meant Jarvis"
 ]
 
 # Function-related inquiries
 function1 = [
-    "function", "length of word", "how long", "length", 
-    "word length", "character count", "how many characters", 
-    "how many characters does this word have", "count letters", 
+    "function", "length of word", "how long", "length",
+    "word length", "character count", "how many characters",
+    "how many characters does this word have", "count letters",
     "word size", "letter count", "measure word", "how long is this word", "how long"
 ]
 
 # To-do list inquiries
 todo = [
-    "what to do", "to do", "to do", "Todo", "To Do", 
-    "my to do list", "tasks", "tasks for today", 
-    "what's on my list", "Jarvis my tasks", "do list", 
-    "Jarvis show tasks", "things to do", "pending tasks", "what are my tasks", "what do I have to do today", "what do I got", "do I have anything today", "anything today", "what do I got to do today"
+    "what to do", "to do", "to do", "Todo", "To Do",
+    "my to do list", "tasks", "tasks for today",
+    "what's on my list", "Jarvis my tasks", "do list",
+    "Jarvis show tasks", "things to do", "pending tasks", "what are my tasks", "what do I have to do today", "what do I got", "do I have anything today", "anything today", "what do I got to do today","whats on my to do list", "what's on my to do list"
 ]
+
+add_todo_phrases = [
+    "add to my to-do list", "add to my list", "put this on my to-do list",
+    "Jarvis add this to my tasks", "can you add this to my list",
+    "remember this task", "save this to my to-do", "include this in my tasks",
+    "note this down", "make a note", "create a task",
+    "add a new task", "put this in my to-do", "Jarvis save this",
+    "add task", "add new task", "write this down",
+    "add this to my agenda", "add to my planner",
+    "add to my schedule", "I need to do this",
+    "remind me to do this", "task for later", "add this for later",
+    "can you save this task", "put this on my list for later",
+    "don't let me forget this", "make sure I do this",
+    "set this as a task", "log this task", "schedule this",
+    "Jarvis put this in my to-do list", "keep track of this",
+    "remember to do this", "track this task", "add a reminder",
+    "remind me about this", "keep this in my to-do list",
+    "schedule a reminder for this", "add this to my daily tasks",
+    "track this for me", "save it to my tasks", "make a list entry", "add to my to do list"
+]
+
 
 # New project creation
 new_project = [
-    "new project", "project", "start a new project", 
-    "Jarvis start a new project", "make a folder", 
-    "new folder", "start a project", "create project", 
+    "new project", "project", "start a new project",
+    "Jarvis start a new project", "make a folder",
+    "new folder", "start a project", "create project",
     "open new project", "set up project", "project creation", "start a new invention", "invention idea", "I have an idea", "let's start building it", "let's start that", "start making that", "make a folder", "start something new", "make a new project"
 ]
 
 # Asking for jokes
 facts = [
-    "tell me a fact", "got any facts", 
-    "funny stuff", "fact please","tell something interesting", 
+    "tell me a fact", "got any facts",
+    "funny stuff", "fact please","tell something interesting",
     "Jarvis fact", "tell me a random fact", "random fact", "fact", "tell me a fact", "ideas"
 ]
 
 # Asking for time or date
 time_date = [
-    "what time is it", "what's the time", "time", 
-    "tell me the time", "current time", "clock", 
-    "what's the date", "date please", "todays date", 
+    "what time is it", "what's the time", "time",
+    "tell me the time", "current time", "clock",
+    "what's the date", "date please", "todays date",
     "what day is it", "day today", "Jarvis time", "give me the time", "what day is it", "what month is it", "what's the date", "month", "date", "give me the time"
 ]
 
 # Exiting or shutting down
 exit_shutdown = [
-    "bye", "goodbye", "exit", "close", "shut down", 
-    "turn off", "Jarvis bye", "see you later", "I'm leaving", 
+    "bye", "goodbye", "exit", "close", "shut down",
+    "turn off", "Jarvis bye", "see you later", "I'm leaving",
     "end session", "terminate", "power down", "power off", "I'll see you", "see ya", "goodbye Jarvis", "see you Jarvis", "power down", "go to sleep", "die", "kill yourself", "I'll see you later", "see you later Jarvis", "goodnight Jarvis", "goodnight", "good night", "good night Jarivs", "see you tomorrow", "see you tommorow Jarvis", "bye Jarvis", "bye", "night", "night Jar", "night Jarvis", "goodnight Jar", "night jar", "goodnight jar", "see ya tmrw", "un plug", "unplug", "talk to you later"
 ]
 
+weather = [
+        "what's the weather", "whats the weather", "weather", "how is it outside", "temperature", "temp", "temperature", "humidity", "whats the humidity", "wind", "wind speed", "is it windy", "is it going to rain", "will it rain", "forcast today", "whats the forcast", "what's the weather looking like"
+]
 
 # Fun responses
 fun = [
-    "dance", "sing", "do something fun", "lets have fun", 
-    "show me something cool", "impress me", "Jarvis do a trick", 
+    "dance", "sing", "do something fun", "lets have fun",
+    "show me something cool", "impress me", "Jarvis do a trick",
     "what can you do thats fun", "party mode", "Jarvis entertain",
     "tell me a joke", "joke", "make me laugh"
 ]
 
 youtube_phrases = [
-        "find a video", "find me a video", "youtube", "youtube video", "get youtube", "I want to find a youtube video", "I want a youtube video", "find me a youtube video", "find youtube", "search youtube", "search for this video", "find video", "find this video", "find youtube video", "find Youtube video", "Youtube", "find Youtube", "Youtube video", "I want to find this video", "im looking for this video"
+        "find a video", "find me a video", "youtube", "youtube video", "get youtube", "I want to find a youtube video", "I want a youtube video", "find me a youtube video", "find youtube", "search youtube", "search for this video", "find video", "find this video", "find youtube video", "find Youtube video", "Youtube", "find Youtube", "Youtube video", "I want to find this video", "im looking for this video", "search youtube"
 ]
+
+news = [
+    "what's in the news", "latest news", "current events",
+    "Jarvis any news", "tell me the headlines", "update me on the world",
+    "news please", "what's happening around the world",
+    "daily news update", "give me today's news", "news", "give me the news", "news please", "what's happening today", "whats the news", "update me on today"
+]
+
+# Motivational quotes
+motivation = [
+    "motivate me", "give me some inspiration", "I need motivation",
+    "inspire me Jarvis", "say something inspiring", "encourage me",
+    "tell me a motivational quote", "lift my spirits",
+    "Jarvis, boost my morale", "give me a pep talk", "motivate me", "motivate me Jarvis", "I need some motivation", "need some motivation", "inspirational quotes", "inspire me", "inspiration", "need a quote"
+]
+
 
 stock_phrases = [
     "stock update",
@@ -121,6 +161,118 @@ stock_phrases = [
     "stock",
     "what are the stocks", "what do the stocks look like", "stock update", "update me on the stocks"
 ]
+
+# User greetings and responses
+greetings.extend([
+    "yo Jarvis", "hey buddy", "what's good Jarvis", "hello there",
+    "hi", "yo what's up", "greetings", "hi there", "yo yo Jarvis",
+    "what's new Jarvis", "hiya", "good to see you Jarvis", "howdy Jarvis"
+])
+
+# Positive responses
+good.extend([
+    "not bad", "doing great", "loving it", "pretty good",
+    "I'm fine", "feeling awesome", "better than ever",
+    "just perfect", "I'm fantastic", "pretty decent", "all good",
+    "everything's fine", "yup", "heck yeah", "totally", "all is well"
+])
+
+# Thank you responses
+thanks.extend([
+    "thanks man", "thanks a bunch", "thanks bro", "thanks a ton",
+    "big thanks", "thanks mate", "cheers Jarvis", "I appreciate it",
+    "grateful", "many thanks", "a big thank you", "super thanks",
+    "massive thanks", "thanks Jarvis appreciate it", "Jarvis thanks a lot"
+])
+
+# Asking how Jarvis is
+howis.extend([
+    "how's it going Jarvis", "how have you been", "how's life",
+    "you alright Jarvis", "you doing okay", "Jarvis how's your day",
+    "how are things", "Jarvis you good", "hows everything",
+    "what's up buddy", "what’s new", "how's your system", "you doing well"
+])
+
+# Negative responses
+bad.extend([
+    "not feeling it", "meh", "could be better", "not in the mood",
+    "kind of bad", "horrible", "terrible", "this sucks", "super annoyed",
+    "really bad", "not okay", "miserable", "worst day ever",
+    "Jarvis this is awful", "no way", "not feeling great"
+])
+
+# Function-related inquiries
+function1.extend([
+    "word count please", "letter measure", "analyze word length",
+    "determine size", "check how long", "what's the size",
+    "Jarvis count characters", "measure the length",
+    "can you count letters", "analyze this word"
+])
+
+# To-do list inquiries
+todo.extend([
+    "list my tasks", "what do I have", "Jarvis show my tasks",
+    "today's tasks", "any new tasks", "pending work",
+    "what needs to be done", "what's left to do", "what do I need to finish",
+    "remind me my tasks"
+])
+
+# New project creation
+new_project.extend([
+    "create a new project", "start something fresh", "begin new project",
+    "open project folder", "build something new", "create an idea",
+    "let's start a plan", "new invention", "let's brainstorm a project"
+])
+
+# Asking for jokes
+facts.extend([
+    "Jarvis tell me something cool", "cool facts", "educate me",
+    "what's something I don't know", "give me knowledge",
+    "learn me something", "Jarvis entertain me with facts"
+])
+
+# Asking for time or date
+time_date.extend([
+    "what's the time Jarvis", "tell me what day it is",
+    "today's month", "calendar date", "Jarvis what's today",
+    "time check", "tell me the time Jarvis"
+])
+
+# Exiting or shutting down
+exit_shutdown.extend([
+    "later Jarvis", "I'm out", "gotta go", "signing off",
+    "Jarvis sleep mode", "shut it down", "power off please",
+    "goodbye my friend", "talk to you later Jarvis"
+])
+
+# Weather inquiries
+weather.extend([
+    "what's the forecast", "any weather updates", "how's the weather looking",
+    "is it going to rain", "Jarvis weather status", "what's outside like",
+    "do I need an umbrella", "is it cold outside"
+])
+
+# Fun responses
+fun.extend([
+    "Jarvis show me something funny", "let's have some fun",
+    "tell me a cool joke", "party mode Jarvis", "make me smile",
+    "do something awesome", "show me a trick"
+])
+
+# YouTube inquiries
+youtube_phrases.extend([
+    "find a cool video", "look up this video", "video search",
+    "grab a YouTube link", "YouTube me this", "Jarvis play a video",
+    "show me this clip", "download this video", "stream a video"
+])
+
+# Stock inquiries
+stock_phrases.extend([
+    "check my stocks", "give me the market update",
+    "how's my portfolio", "update me on the market", "latest finance news",
+    "finance check", "market insights", "stock summary"
+])
+
 
 
 random_facts = [
@@ -154,4 +306,37 @@ random_facts = [
     "You can’t hum while holding your nose closed.",
     "The heart of a blue whale is so large that a human could swim through its arteries.",
     "There are more fake flamingos in the world than real ones."
+]
+
+motivational_quotes = [
+    "Believe you can and you're halfway there. – Theodore Roosevelt",
+    "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",
+    "Don't watch the clock; do what it does. Keep going. – Sam Levenson",
+    "The only way to do great work is to love what you do. – Steve Jobs",
+    "Act as if what you do makes a difference. It does. – William James",
+    "Your limitation—it's only your imagination.",
+    "Push yourself, because no one else is going to do it for you.",
+    "Great things never come from comfort zones.",
+    "Dream it. Believe it. Build it.",
+    "Success doesn’t just find you. You have to go out and get it.",
+    "The harder you work for something, the greater you’ll feel when you achieve it.",
+    "Don't stop when you're tired. Stop when you're done.",
+    "Wake up with determination. Go to bed with satisfaction.",
+    "Do something today that your future self will thank you for.",
+    "Little things make big days.",
+    "It’s going to be hard, but hard does not mean impossible.",
+    "Success doesn’t happen overnight. Keep pushing forward.",
+    "Stay positive, work hard, and make it happen.",
+    "Failure is the opportunity to begin again more intelligently. – Henry Ford",
+    "Difficult roads often lead to beautiful destinations.",
+    "Believe in yourself and all that you are.",
+    "The secret of getting ahead is getting started. – Mark Twain",
+    "Do what you can, with what you have, where you are. – Theodore Roosevelt",
+    "Start where you are. Use what you have. Do what you can. – Arthur Ashe",
+    "You are capable of amazing things.",
+    "Strive for progress, not perfection.",
+    "Success is not how high you have climbed, but how you make a positive difference in the world.",
+    "Every day may not be good, but there is something good in every day.",
+    "Doubt kills more dreams than failure ever will.",
+    "Your time is limited, so don't waste it living someone else's life. – Steve Jobs"
 ]
