0e5bf7f87495d5aeb562a21b04a864b58a19be3e
ryperson <80601374+ry207@users.noreply.github.com>
2025-01-27 06:55:04 -0600

    Update journal.py

--- journal.py
+++ journal.py
@@ -1,4 +1,5 @@
 import datetime
+from colorama import Fore
 
 def addToJournal(journal_entry):
 
@@ -13,3 +14,28 @@ def addToJournal(journal_entry):
         print("Wrote to Journal.txt")
     except:
         print("An error may have occured.")
+
+def addToDo(todo_entry):
+
+    date = input("Date(M/D/Y): ")
+    time = input("Time(XX:XX XM): ")
+
+    try:
+        with open("Todo.txt", "a") as f:
+            f.write(f"{date} at {time}:  {todo_entry}\n")
+        print("Wrote to Todo.txt")
+    except:
+        print("An error may have occured.")
+
+def getToDo():
+    count = 0
+    x = datetime.datetime.now()
+    date = x.strftime("%x")
+    with open("Todo.txt", "r") as f:
+        for line in f:
+            if date in (f" {line} "):
+               print(Fore.GREEN + line + Fore.WHITE)
+               count += 1
+            else:
+                print(line)
+        print(f"{Fore.CYAN}You have {Fore.RED}{count}{Fore.CYAN} things to do today.{Fore.WHITE}")
