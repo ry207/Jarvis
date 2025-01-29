2127b11e00582e5e2322ed4cf4663009b4d0e067
ryperson <80601374+ry207@users.noreply.github.com>
2025-01-29 14:23:44 +0000

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -7,27 +7,37 @@ engine = pyttsx3.init()
 
 def getstocks():
     try:
-        url  = f"https://investing.com/"
+        url  = f"https://finance.yahoo.com/"
         print(f"searching at: {url}")
         page = requests.get(url).text
         soup = BeautifulSoup(page, "lxml")
-        content = soup.find("table", class_="datatable-v2_table__93S4Y dynamic-table-v2_dynamic-table__iz42m datatable-v2_table--mobile-basic__uC0U0 datatable-v2_table--freeze-column__uGXoD datatable-v2_table--freeze-column-first__zMZNN undefined")
+        content = soup.find("li", class_="dock-item dockRevealTarget dockRevealTarget-5 yf-1doyjox")
     except:
         print("Check your internet.")
 
-    
 
-    blocks = content.find_all('tr')
-    for block in reversed(blocks[1:(len(blocks))]):
-        tds = block.find_all('td')
-        for td in (tds):
-            try:
-                print(td.text)
-            except:
-                print("Error")
-        name = block.find('td', class_="datatable-v2_cell__IwP1U !h-auto w-full py-2 mdMax:border-r dynamic-table-v2_col-name__Xhsxv !py-2")
-        price = block.find('td', class_="datatable-v2_cell__IwP1U dynamic-table-v2_col-other__zNU4A text-right rtl:text-right")
-        percentChange = block.find('td', class_="datatable-v2_cell__IwP1U datatable-v2_cell--up__lVyET datatable-v2_cell--bold__cXQUV dynamic-table_col-other__Eu_RC text-right font-bold rtl:text-right")
+
+    blocks = content.find_all('li', class_='dock-item primary font-default yf-46ugf5 clickability hover hasSymbolOptions')
+    print("----------------------------------")
+    for block in blocks:
+        name = block.find('span', class_="longName yf-pt5nkw")
+        price = block.find('fin-streamer', class_="last-price yf-pt5nkw")
+        percentChange = block.find('span', class_="percent-change txt-positive yf-pt5nkw")
+        if name != None:
+            name = name.text
+            print(f"Company: {name}")
+        if price != None:
+            price = price.text
+            print(f"Price: ${price}")
+        else:
+            print("No price")
+        if percentChange != None:
+            percentChange = percentChange.text
+            print(f"Change: {percentChange}")
+        else:
+            print("No change")
         print("----------------------------------")
-    engine.say(f"{name.text} has a price of {price.text} dollars.")
+    engine.say(f"{name} has a price of {price} dollars.")
     engine.runAndWait()
+
+getstocks()
